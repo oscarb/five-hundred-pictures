866d7c8aadb28587ea666c660be3ba28101ecb56
oscarb <git@oscarb.se>
2016-11-14 17:24:02 +0100

    Remove FAB

--- app/src/main/java/se/oscarb/fivehundredpictures/MainActivity.java
+++ app/src/main/java/se/oscarb/fivehundredpictures/MainActivity.java
@@ -2,8 +2,6 @@ package se.oscarb.fivehundredpictures;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -42,15 +40,6 @@ public class MainActivity extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         setSupportActionBar(binding.toolbar);
-
-        FloatingActionButton fab = binding.fab;
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         // Create Retrofit instance
         Retrofit builder = new Retrofit.Builder()
